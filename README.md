43a94da7bbc5e751800fc8d7304d2f2fdbfba25c
Andrzej Stasiński <andista@wp.pl>
2020-10-14 12:14:29 +0200

    initial app

---
+++ README.md
@@ -0,0 +1,18 @@
+## Simple Slider in JavaScript
+
+#### What does the project do - cyclic animation of pictures
+
+#### Features
+
+* solution in JavaScript - very simple and short code
+* picture animation on picture TV 
+* simple code CSS 
+
+
+
+
+
+
+
+
+
---
+++ script.js
@@ -0,0 +1,31 @@
+// Solition 1
+// -----------------------
+
+var myScreen = document.getElementById('myScreen');
+var tab = ['up1.jpg', 'up2.jpg', 'up3.jpg'];
+var nr = 0;
+function loop() {
+    myScreen.innerHTML = "<img src='"+tab[nr]+"' />";
+    nr++;
+    if(nr >= tab.length) nr=0;
+    setTimeout('loop()', 2000);
+}
+loop();
+
+
+// Solition 2
+// -----------------------
+
+// var myScreen = document.getElementById('myScreen');
+// var myPics = ['up1.jpg','up2.jpg','up3.jpg'];
+// var totalPics = myPics.length;
+// var i = 0;
+// function loop() {
+// 	if(i > (totalPics - 1)){
+// 		i = 0;
+// 	}
+// 	myScreen.innerHTML = '<img src="'+myPics[i]+'">';
+// 	i++;
+// 	var loopTimer = setTimeout('loop()',3000);
+// }
+// loop();
